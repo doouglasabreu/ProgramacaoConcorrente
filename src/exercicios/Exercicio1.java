5863430f4f8f62aba8fa7802d14b47e5c4b1bbd3
DOUGLAS VINICIUS DE ABREU <a1710648@debian9-dacom.dacom>
2018-08-13 21:37:00 -0300

    inserindo codigo java

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author a1710648
+ */
+public class Exercicio1 {
+    int a;
+    
+    
+}
